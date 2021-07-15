828723270cb2dfe019cf5e51c0d5e05e66bf4880
Ramdhan Ramadhian ID <75580821+Ramdhan7@users.noreply.github.com>
2021-07-16 00:24:54 +0700

    Update premier.py

--- premier.py
+++ premier.py
@@ -34,7 +34,7 @@ else:
 ### HEADERS ###
 
 def banner():
-    print("""\x1b[0;35m   ___  _                         __           Multi Brute Force
+    print("""\x1b[0;35m   ___  _                         __           
   / _ \(_)\x1b[0;35mMulti Brute Force ___  ___/ /\x1b[0;36mPrivate
 \x1b[0;35m/ // / / _ `/  ' \/ _ \/ _ \/ _  /\x1b[0;36mCreated By 
 /____/_/\_,_/_/_/_/\___/_//_/\_,_/\x1b[0;36mRamdhan R""")
